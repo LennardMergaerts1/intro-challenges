d4d6e7915446891ab6138a7742f0b02af395afa9
Lennard Mergaerts <lennard.mergaerts@gmail.com>
2023-11-19 23:28:57 +0100

    laatste changes

--- 3. programming-challenges/01-programming-challenge/challenge-files/student.js
+++ 3. programming-challenges/01-programming-challenge/challenge-files/student.js
@@ -209,3 +209,151 @@ function firstLeft(car) {
     turnLeft(car);
     forwardUntilWall(car);
 }
+function zigZag(car) {
+    firstRight(car);
+    turnLeft(car);
+    forward(car);
+    firstLeft(car);
+}
+
+function forwardUntilFreeRight(car) {
+    while (sensorRight(car)) {
+        forward(car);
+    }
+}
+
+function secondRight(car) {
+    forwardUntilFreeRight(car);
+    forward(car);
+    forwardUntilFreeRight(car);
+    turnRight(car);
+    forwardUntilWall(car);
+}
+
+function thirdRight(car) {
+    forwardUntilFreeRight(car);
+    forward(car);
+    forwardUntilFreeRight(car);
+    forward(car);
+    forwardUntilFreeRight(car);
+    turnRight(car);
+    forwardUntilWall(car);
+}
+function forwardUntilNthRight(car, nrights) {
+    let i = nrights;
+
+    while (i > 0) {
+        forward(car);
+
+        if (!sensorRight(car)) {
+            i = i - 1;
+        }
+    }
+}
+
+function fourthRight(car) {
+    forwardUntilNthRight(car, 4);
+    turnRight(car);
+    forwardUntilWall(car);
+}
+
+function forwardUntilNthLeft(car, nlefts) {
+    let i = nlefts;
+
+    while (i > 0) {
+        forward(car);
+
+        if (!sensorLeft(car)) {
+            i = i - 1;
+        }
+    }
+}
+
+function fifthLeft(car) {
+    forwardUntilNthLeft(car, 5);
+    turnLeft(car);
+    forwardUntilWall(car);
+}
+
+function maze(car) {
+    R(2);
+    L(1);
+    L(2);
+    L(2);
+    R(4);
+    R(1);
+    L(3);
+    forwardUntilWall(car);
+
+
+    function L(n) {
+        forwardUntilNthLeft(car, n);
+        turnLeft(car);
+    }
+
+    function R(n) {
+        forwardUntilNthRight(car, n);
+        turnRight(car);
+    }
+}
+function forwardUntilDestination(car) {
+    while (!destinationReached(car)) {
+        forward(car);
+    }
+}
+
+function isDeadEnd(car) {
+    if (!sensor(car)) {
+        return false;
+    }
+
+    if (!sensorRight(car)) {
+        return false;
+    }
+
+    if (!sensorLeft(car)) {
+        return false;
+    }
+
+    return true;
+}
+
+function turnAround(car) {
+    turnRight(car);
+    turnRight(car);
+}
+
+function backward(car) {
+    turnAround(car);
+    forward(car);
+    turnAround(car);
+}
+
+function findDeadEnd(car) {
+    while (true) {
+        forward(car);
+
+        if (isDeadEnd(car)) {
+            return;
+        }
+
+        backward(car);
+        turnRight(car);
+    }
+}
+
+function incompleteU(car) {
+    forwardUntilWall(car);
+    turnRight(car);
+    forwardUntilWall(car);
+    turnRight(car);
+    forwardUntilWall(car);
+}
+
+function whichDirection(car) {
+    while (sensor(car)) {
+        turnRight(car);
+    }
+
+    forwardUntilWall(car);
+}
